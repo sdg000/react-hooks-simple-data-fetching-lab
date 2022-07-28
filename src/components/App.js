6d73b4ab2b3c0dce04445452f56e82c5dc2d369d
Seth David Gyimah <esec26@gmail.com>
2022-07-28 19:37:59 +0000

    simple data fetching lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+function App(){
+    //saving image variable to state
+    const [image, setImage] = useState("")
+
+    //fetching data from API after DOM renders
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(function(r){
+            return r.json()
+        })
+        .then(function(data){
+            setImage(data.message)
+            //console.log(data.message)
+        })
+        
+    }, [])
+
+    // fetch asynchronous wait-message
+    if(!image){
+        return <p>Loading...</p>
+    }
+
+    return (
+       <div>
+         <img src={image} alt="A Random Dog"/>
+       </div>
+    )
+}
+
+export default App
